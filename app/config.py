c5693a9d7772b721e31c407eb2d0f960c8a2ce16
MouhamadouGaye <99024541+MouhamadouGaye@users.noreply.github.com>
2024-11-20 20:10:29 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,20 +2,20 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     SECRET_KEY: str  # This will hold your secret key
-    ALGORITHM: str ="HS256" # This sets the default algorithm
-    DATABASE_URL:str
+    ALGORITHM: str = "HS256"  # This sets the default algorithm
+    DATABASE_URL: str
     MY_GMAIL_SECRET_KEY_CONTACT: str  # Add this field if you need it
     POSTGRES_HOST: str = "localhost"  # Default value if not provided
     POSTGRES_PORT: int = 5433  # Set to your custom PostgreSQL port (5433)
-    PG_PASSWORD= str
-    PG_DB= str
+    PG_PASSWORD: str  # Fixed annotation
+    PG_DB: str  # Fixed annotation
 
     class Config:
         env_file = ".env"  # This allows you to load settings from a .env file
   
 
 # Create an instance of the Settings class
-# settings = Settings()
+settings = Settings()
 
 
 
