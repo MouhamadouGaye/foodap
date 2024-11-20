bc01ace74baf650e20034414a8b4841f4ea56b83
MouhamadouGaye <99024541+MouhamadouGaye@users.noreply.github.com>
2024-11-20 15:30:58 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,11 +5,31 @@ class Settings(BaseSettings):
     ALGORITHM: str ="HS256" # This sets the default algorithm
     DATABASE_URL:str
     MY_GMAIL_SECRET_KEY_CONTACT: str  # Add this field if you need it
+    POSTGRES_USER = str
+    PG_PASSWORD= str
+    PG_DB= str
 
     class Config:
         env_file = ".env"  # This allows you to load settings from a .env file
+  
 
 # Create an instance of the Settings class
-settings = Settings()
+# settings = Settings()
 
 
+
+# from pydantic import BaseSettings
+
+# class Settings(BaseSettings):
+#     postgres_user: str
+#     pg_password: str
+#     pg_db: str
+
+#     class Config:
+#         env_file = ".env"
+#         env_file_encoding = "utf-8"
+#         env = {
+#             'postgres_user': 'POSTGRES_USER',
+#             'pg_password': 'POSTGRES_PASSWORD',
+#             'pg_db': 'POSTGRES_DB',
+#         }
